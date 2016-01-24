0c461758055d2505b863e7662e770356706c2ff9
grimm <chris.grimm@gmail.com>
2016-01-24 15:13:16 -0500

    added autofac registration

--- API/Rinita.Stephan/Controllers/RSVPController.cs
+++ API/Rinita.Stephan/Controllers/RSVPController.cs
@@ -12,11 +12,6 @@ namespace Rinita.Stephan.Controllers
 	{
         private readonly IPostHelper _postHelper;
 
-	    public RSVPController()
-	    {
-		    
-	    }
-
         public RSVPController(IPostHelper postHelper)
         {
             _postHelper = postHelper;
--- API/Rinita.Stephan/Global.asax.cs
+++ API/Rinita.Stephan/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+﻿using System.Web.Http;
 
 namespace Rinita.Stephan
 {
@@ -12,6 +7,8 @@ namespace Rinita.Stephan
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+	        AutofacConfig.Start();
         }
     }
 }
